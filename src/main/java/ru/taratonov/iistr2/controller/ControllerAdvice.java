3f8b6ede3e9af58b6dd5f0f9466fa79be594d6ea
Vadim Taratonov <taratonovv8@gmail.com>
2023-11-26 13:11:54 +0300

    update code

--- src/main/java/ru/taratonov/iistr2/controller/ControllerAdvice.java
+++ src/main/java/ru/taratonov/iistr2/controller/ControllerAdvice.java
@@ -1,11 +1,16 @@
 package ru.taratonov.iistr2.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.taratonov.iistr2.model.Error;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -14,5 +19,14 @@ public class ControllerAdvice {
     public Error catchException(HttpMessageNotReadableException exception) {
         String[] split = exception.getMessage().split(":");
         return new Error(String.join(" ", split[split.length - 2], split[split.length - 1]), LocalDateTime.now());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public List<Error> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<Error> errors = new ArrayList<>();
+        ex.getBindingResult().getFieldErrors().forEach(e ->
+                errors.add(new Error(e.getField() + " " + e.getDefaultMessage(), LocalDateTime.now())));
+        return errors;
     }
 }
